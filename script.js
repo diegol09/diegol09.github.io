79974861ad4f84b2c9519a91005918b2c45a09cc
diegol09 <93894471+diegol09@users.noreply.github.com>
2021-11-07 21:03:40 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var forma = document.getElementById ("forma"),
+resultado = document.getElementById("resultado");
+forma.addEventListener("submit",procesa,false);
+
+
+
+function procesa(){
+var base =parseInt( forma["base"].value);
+var res=Math.sqrt(base);
+forma["resultado"].value= "El Resultado es :"+res;
+
+}
